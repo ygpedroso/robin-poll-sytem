3242dc208f6ef68a099a4a6519e1771bec8b7b38
ygpedroso <ygpedroso2503@gmail.com>
2018-08-28 14:03:02 -0400

    Create database configuration

---
+++ src/config/database.js
@@ -0,0 +1,23 @@
+import { ok } from 'assert';
+import mongoose from 'mongoose';
+import logger from 'winston-color';
+
+const initDB = () => {
+	try {
+		const mongoUrl = process.env.DATABASE_URL;
+		ok(mongoUrl, 'Missing environment variable for DATABASE_URL');
+		mongoose
+			.connect(
+				mongoUrl,
+				{
+					useNewUrlParser: true,
+				}
+			)
+			.then(() => logger.info('Database connection established'))
+			.catch(e => logger.error(e.message));
+	} catch (e) {
+		console.error(e.message);
+	}
+};
+
+export default initDB;
