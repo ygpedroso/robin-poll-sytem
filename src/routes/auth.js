7dfeb0a8717adeda2b6a33d29765e0a85a7330aa
ygpedroso <ygpedroso2503@gmail.com>
2018-08-28 15:03:19 -0400

    Provide implementation for the register enpoint

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,29 @@
 import express from 'express';
+import { check, validationResult } from 'express-validator/check';
+import User from './../models/User';
+
 const router = express.Router();
 
-router.get('/register', function(req, res) {
-	res.boom.notFound();
-});
+router.post(
+	'/register',
+	[check('email').isEmail(), check('password').isLength({ min: 5 })],
+	(req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			res.boom.badRequest(errors.array());
+		}
+		User.create(
+			{
+				email: req.body.email,
+				password: req.body.password,
+			},
+			(err, user) => {
+				if (err) res.boom.badImplementation(err);
+				return res.status(200).send();
+			}
+		);
+	}
+);
 
 router.get('/login', function(req, res) {
 	res.boom.notFound();
