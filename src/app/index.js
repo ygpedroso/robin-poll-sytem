9a54c68e0ff51bae5d874d288b10fc91b42c0861
ygpedroso <ygpedroso2503@gmail.com>
2018-08-28 14:23:22 -0400

    Register auth related routes in the express app

--- src/app/index.js
+++ src/app/index.js
@@ -3,6 +3,7 @@ import morgan from 'morgan';
 import cors from 'cors';
 import boom from 'express-boom';
 import initDB from './../config/database';
+import authRouter from '../routes/auth';
 import usersRouter from './../routes/users';
 
 initDB();
@@ -14,6 +15,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/users', usersRouter);
+app.use('api/v1/auth', authRouter);
+app.use('api/v1/users', usersRouter);
 
 export default app;
