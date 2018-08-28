0ff092c0915dbdefffa2877177764641fb1eb295
ygpedroso <ygpedroso2503@gmail.com>
2018-08-28 14:10:23 -0400

    Create the User model

---
+++ src/models/User.js
@@ -0,0 +1,49 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const Schema = mongoose.Schema;
+export const UserSchemaName = 'User';
+const SALT_WORK_FACTOR = 10;
+
+const UserSchema = new Schema(
+	{
+		email: {
+			type: String,
+			required: true,
+			lowercase: true,
+			index: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+	},
+	{ timestamps: true }
+);
+
+UserSchema.pre('save', function(next) {
+	const user = this;
+	if (!user.isModified('password')) return next();
+
+	bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+		if (err) return next(err);
+
+		bcrypt.hash(user.password, salt, function(err, hash) {
+			if (err) return next(err);
+
+			user.password = hash;
+			next();
+		});
+	});
+});
+
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+	const user = this;
+	bcrypt.compare(candidatePassword, user.password, function(err, isMatch) {
+		if (err) return cb(err);
+		cb(null, isMatch);
+	});
+};
+
+export default mongoose.model(UserSchemaName, UserSchema);
