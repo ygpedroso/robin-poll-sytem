0eed2b9e7c956e44c327324ec480838dde7d5d71
ygpedroso <ygpedroso2503@gmail.com>
2018-08-29 14:53:26 -0400

    Ask explicitly for the password on the local strategy

--- src/config/passport-local.js
+++ src/config/passport-local.js
@@ -9,23 +9,25 @@ passport.use(
 			passwordField: 'password',
 		},
 		(email, password, done) => {
-			User.findOne({ email }, (err, user) => {
-				if (err) {
-					return done(err);
-				}
-				if (!user) {
-					return done(null, false, { message: 'Incorrect username.' });
-				}
-				user.comparePassword(password, (err, isMatch) => {
+			User.findOne({ email })
+				.select('+password')
+				.exec((err, user) => {
 					if (err) {
 						return done(err);
 					}
-					if (!isMatch) {
-						return done(null, false, { message: 'Incorrect password.' });
+					if (!user) {
+						return done(null, false, { message: 'Incorrect username.' });
 					}
-					return done(null, user);
+					user.comparePassword(password, (err, isMatch) => {
+						if (err) {
+							return done(err);
+						}
+						if (!isMatch) {
+							return done(null, false, { message: 'Incorrect password.' });
+						}
+						return done(null, user);
+					});
 				});
-			});
 		}
 	)
 );
