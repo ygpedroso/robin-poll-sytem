3bf4c063f0e9f7526ddb3b5cc2fa41633a0898d8
ygpedroso <ygpedroso2503@gmail.com>
2018-08-28 23:27:16 -0400

    Provide implementation for the login endpoint

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import passport from 'passport';
+import jwt from 'jsonwebtoken';
 import { body, validationResult } from 'express-validator/check';
 import User from './../models/User';
 
-const router = express.Router();
+const router = express.Router({});
 
 router.post(
 	'/register',
@@ -19,7 +21,7 @@ router.post(
 				},
 				(err, user) => {
 					if (err) {
-						res.boom.conflict(err);
+						res.status(500).json({ error: err });
 					} else {
 						res.status(200).json({
 							user: user.email,
@@ -31,12 +33,31 @@ router.post(
 	}
 );
 
-router.get('/login', function(req, res) {
-	res.boom.notFound();
-});
-
-router.get('/logout', function(req, res) {
-	res.boom.notFound();
+router.post('/login', function(req, res, _) {
+	passport.authenticate('local', { session: false }, (err, user, info) => {
+		if (err) {
+			return res.status(500).json({
+				message: 'Something went wrong',
+				err,
+			});
+		}
+		if (!user) {
+			return res.status(400).json({
+				message: info.message,
+			});
+		}
+		req.login(user, { session: false }, err => {
+			if (err) {
+				return res.send(err);
+			}
+			const payload = {
+				id: user.id,
+				email: user.email,
+			};
+			const token = jwt.sign(payload, process.env.JWT_SECRET);
+			return res.json({ token });
+		});
+	})(req, res);
 });
 
 export default router;
