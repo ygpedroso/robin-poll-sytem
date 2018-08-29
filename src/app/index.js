188f59f907c1eb7c8583b9e600abc2bf2cb46f93
ygpedroso <ygpedroso2503@gmail.com>
2018-08-29 02:33:08 -0400

    Register the votes router in the application

--- src/app/index.js
+++ src/app/index.js
@@ -8,6 +8,7 @@ import authRouter from '../routes/auth';
 import usersRouter from './../routes/users';
 import pollsRouter from './../routes/polls';
 import pollsOptionsRouter from './../routes/pollsOptions';
+import votesRouter from './../routes/votes';
 import './../config/passport-local';
 import './../config/passport-jwt';
 
@@ -22,7 +23,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', passport.authenticate('jwt', { session: false }), usersRouter);
-app.use('/api/v1/polls', pollsRouter, pollsOptionsRouter);
+app.use('/api/v1/polls', pollsRouter, pollsOptionsRouter, votesRouter);
 
 app.use((_, res, next) => {
 	res.boom.notFound();
