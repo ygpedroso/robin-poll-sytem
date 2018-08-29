60ee5720ab8feedcd62faa78cd8bf168adb0d71b
ygpedroso <ygpedroso2503@gmail.com>
2018-08-28 23:46:59 -0400

    Secure the users endpoint

--- src/app/index.js
+++ src/app/index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import boom from 'express-boom';
+import passport from 'passport';
 import initDB from './../config/database';
 import authRouter from '../routes/auth';
 import usersRouter from './../routes/users';
 import pollsRouter from './../routes/polls';
 import './../config/passport-local';
+import './../config/passport-jwt';
 
 initDB();
 const app = express();
@@ -18,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/users', usersRouter);
+app.use('/api/v1/users', passport.authenticate('jwt', { session: false }), usersRouter);
 app.use('/api/v1/polls', pollsRouter);
 
 app.use((_, res, next) => {
