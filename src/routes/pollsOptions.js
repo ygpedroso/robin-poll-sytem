4dba8fce18813b0a69b72eaa3df4d3283ec2952d
ygpedroso <ygpedroso2503@gmail.com>
2018-08-29 00:52:06 -0400

    Create endpoint for creating a poll option

--- src/routes/pollsOptions.js
+++ src/routes/pollsOptions.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import Poll from '../models/Poll';
 import PollOption from '../models/PollOption';
+import { body, validationResult } from 'express-validator/check/index';
+import passport from 'passport/lib/index';
+
 const router = express.Router({});
 
-router.get('/', (req, res) => {
+router.get('/:pollId/options', (req, res) => {
+	console.log(req.params.pollId);
 	PollOption.find({ pollId: req.params.pollId }, (err, pollOptions) => {
 		if (err) {
 			return res.status(500).json({
@@ -14,5 +18,40 @@ router.get('/', (req, res) => {
 		return res.send(pollOptions);
 	});
 });
+
+router.post(
+	'/:pollId/options',
+	passport.authenticate('jwt', { session: false }),
+	[body('value').isLength({ min: 1 })],
+	(req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			res.status(400).json({ errors: errors.array() });
+		} else {
+			Poll.findById(req.params.pollId, (err, poll) => {
+				if (err) {
+					return res.status(500).json({
+						message: 'Something went wrong',
+						err,
+					});
+				}
+				console.log(req.params.pollId);
+				console.log(poll);
+				PollOption.create(
+					{ value: req.body.value, pollId: poll.id, createdBy: req.user.id },
+					(err, pollOption) => {
+						if (err) {
+							return res.status(500).json({
+								message: 'Something went wrong',
+								err,
+							});
+						}
+						return res.send(pollOption);
+					}
+				);
+			});
+		}
+	}
+);
 
 export default router;
